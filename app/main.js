ae0fca88bae3850ad8995715729f4a39719c6bdd
angry parrot <quanyouhui901@gmail.com>
2025-04-27 20:45:34 +0800

    实现了open file 功能

--- app/main.js
+++ app/main.js
@@ -1,29 +1,72 @@
 const {
     app,
-    BrowserWindow
-} = require('electron');
+    BrowserWindow,
+    dialog,
+    ipcMain
+} = require("electron");
+const fs = require("fs");
+
 
 let mainWindow = null;
 
-app.on('ready', () => {
-
+app.on("ready", () => {
     mainWindow = new BrowserWindow({
         show: false,
         webPreferences: {
             nodeIntegration: true, // 启用 Node.js 集成
             contextIsolation: false, // 禁用上下文隔离（否则仍无法访问）
-
-        }
+        },
     });
 
-    mainWindow.loadFile('app/index.html');
+    mainWindow.loadFile("app/index.html");
 
     // show the window when ready
-    mainWindow.once('ready-to-show', () => {
+    mainWindow.once("ready-to-show", () => {
         mainWindow.show();
     });
 
-    mainWindow.on('closed', () => {
+    mainWindow.on("closed", () => {
         mainWindow = null;
     });
 });
+
+const getFileFromUser = async () => {
+    const {
+        canceled,
+        filePaths
+    } = await dialog.showOpenDialog(mainWindow, {
+        properties: ["openFile"],
+        filters: [{
+                name: "Markdown",
+                extensions: ["md", "markdown"],
+            },
+            {
+                name: "Text",
+                extensions: ["txt"],
+            },
+        ],
+    });
+    if (canceled || filePaths.length === 0) {
+        return;
+    }
+    const file = filePaths[0];
+    return file;
+};
+
+
+// 打开文件
+ipcMain.handle('open-file', async (event) => {
+    // ...你的 getFileFromUser 逻辑...
+    const file = await getFileFromUser();
+    const content = fs.readFileSync(file).toString();
+    // 返回内容
+    return {
+        file,
+        content
+    }
+});
+
+
+module.exports = {
+    getFileFromUser
+}
--- app/renderer.js
+++ app/renderer.js
@@ -3,16 +3,29 @@ const {
     marked
 } = require('marked');
 
+const {
+    ipcRenderer
+} = require('electron')
 //获取DOM元素
 const markdownView = document.querySelector('#markdown');
 const htmlView = document.querySelector('#html');
+const openFileButton = document.querySelector('#open-file');
+const saveFileButton = document.querySelector('#save-file');
 
 const rendererMarkdownToHTML = (markdown) => {
-    htmlView.innerHTML = marked(markdown, {
-        sanitize: true
-    });
+    htmlView.innerHTML = marked.parse(markdown);
 }
 //监听markdown的变化
 markdownView.addEventListener('keyup', () => {
     rendererMarkdownToHTML(markdownView.value);
+});
+
+openFileButton.addEventListener('click', async () => {
+    const result = await ipcRenderer.invoke('open-file');
+    if (result) {
+        markdownView.value = result.content;
+        rendererMarkdownToHTML(result.content);
+        // 你可以把 result.file 存起来，后续保存用
+        window.currentFile = result.file;
+    }
 });
